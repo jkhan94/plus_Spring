35ebc840eb302c240059a35ae81982cd04790296
joon <tjrwns3428@gmail.com>
2024-06-19 21:10:34 +0900

    feat : UserRoleEnum,UserStauts Enum형 추가 / JwtAuthenticationFilter 필터 추가 / UserDetailsImpl,UserDetailsServcieImpl 상속체 추가

---
+++ src/main/java/com/sparta/easyspring/auth/security/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.sparta.easyspring.auth.security;
+
+import com.sparta.easyspring.auth.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j(topic ="JWT 인증")
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String token = null;
+
+
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            log.info("토큰 유효성 검증");
+            log.info("authorizationHeader :"+authorizationHeader);
+            token = authorizationHeader.substring(7);
+            username = jwtUtil.getUsernameFromToken(token);
+            log.info("username : "+username);
+        }
+
+        log.info("사용자 정보 검증");
+        // 사용자 이름 존재, 현재 SecurityContextHolder에 인증 정보가 없는 경우
+        // statless상태기 때문에 HTTP요청마다 SecurityContextHolder에 인증 정보를 저장해 줘야 됨.
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetailsImpl userDetailsImpl = this.userDetailsService.loadUserByUsername(username);
+
+            log.info("토큰 유효성 재 검증");
+            if(jwtUtil.validateToken(token,userDetailsImpl)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                        userDetailsImpl,null,userDetailsImpl.getAuthorities()
+                    );
+                usernamePasswordAuthenticationToken
+                    .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        log.info("인증 성공");
+        filterChain.doFilter(request,response);
+    }
+}
---
+++ src/main/java/com/sparta/easyspring/auth/security/UserDetailsImpl.java
@@ -0,0 +1,64 @@
+package com.sparta.easyspring.auth.security;
+
+import com.sparta.easyspring.auth.entity.User;
+import com.sparta.easyspring.auth.entity.UserRoleEnum;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user){
+        this.user = user;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        UserRoleEnum userRole = user.getUserRole();
+        String authority = userRole.getRole();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
