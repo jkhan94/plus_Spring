16fb12ff90bac775ea0e5beb81e6a4bb30bef27c
jkhan94 <hjkwork@naver.com>
2024-06-19 13:32:31 +0900

    Add : 컨트롤러 예외처리를 위한 공통 코드 추가
    GlobalExceptionHandler, CustomException, ErrorEnum

---
+++ src/main/java/com/sparta/easyspring/exception/ErrorEnum.java
@@ -0,0 +1,27 @@
+package com.sparta.easyspring.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public enum ErrorEnum {
+    // user
+    USER_NOT_FOUND(400, "등록되지 않은 사용자입니다."),
+
+    // post
+    POST_NOT_FOUND(400, "등록되지 않은 게시글입니다."),
+
+    // comment
+    COMMENT_NOT_FOUND(400, "등록되지 않은 댓글입니다."),
+
+    // like
+    LIKE_NOT_FOUND(400, "좋아요가 설정되지 않았습니다."),
+    CANNOT_LIKE_OWN_CONTENT(400, "본인이 작성한 콘텐츠에 좋아요를 남길 수 없습니다."),
+    DUPLICATE_LIKE(400, "같은 콘텐츠에는 사용자당 한 번만 좋아요가 가능합니다.");
+
+    int statusCode;
+    String msg; // 출력 메시지
+}
