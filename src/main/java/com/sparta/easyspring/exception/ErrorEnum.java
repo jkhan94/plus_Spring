c4a6d94486bed21af68925623e315bc7ebe0bde7
jkhan94 <hjkwork@naver.com>
2024-06-19 17:49:44 +0900

    update : ErrorEnum 추가
    USER_NOT_AUTHENTICATED 추가

--- src/main/java/com/sparta/easyspring/exception/ErrorEnum.java
+++ src/main/java/com/sparta/easyspring/exception/ErrorEnum.java
@@ -19,6 +19,7 @@ public enum ErrorEnum {
 
     // like
     LIKE_NOT_FOUND(400, "좋아요가 설정되지 않았습니다."),
+    USER_NOT_AUTHENTICATED(400, "인증되지 않은 사용자입니다. 로그인해주세요."),
     CANNOT_LIKE_OWN_CONTENT(400, "본인이 작성한 콘텐츠에 좋아요를 남길 수 없습니다."),
     DUPLICATE_LIKE(400, "같은 콘텐츠에는 사용자당 한 번만 좋아요가 가능합니다.");
 
