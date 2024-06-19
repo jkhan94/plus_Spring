a35118ce16c3138973eb04fa41b48419fb0172ce
joon <tjrwns3428@gmail.com>
2024-06-19 11:30:08 +0900

    init: git init

---
+++ src/test/java/com/sparta/easyspring/EasySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.easyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
