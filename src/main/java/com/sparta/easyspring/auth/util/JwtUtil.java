b3dc9b9d5803e0bf7d9ec1a56f03a38698d3fe12
joon <tjrwns3428@gmail.com>
2024-06-19 21:13:53 +0900

    test : 개발용 SecurityConfig 설정추가

--- src/main/java/com/sparta/easyspring/auth/util/JwtUtil.java
+++ src/main/java/com/sparta/easyspring/auth/util/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.sparta.easyspring.auth.util;
 
 import com.sparta.easyspring.auth.config.JwtConfig;
+import com.sparta.easyspring.auth.security.UserDetailsImpl;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -16,7 +17,7 @@ public class JwtUtil {
     private final long refreshTokenExpiration;
     private final SecretKey secretKey;
 
-    public JwtUtil(JwtConfig jwtConfig){
+    public JwtUtil(JwtConfig jwtConfig) {
         this.tokenExpiration = jwtConfig.getTokenExpiration();
         this.refreshTokenExpiration = jwtConfig.getRefreshTokenExpiration();
         this.secretKey = Keys.hmacShaKeyFor(jwtConfig.getSecretKey().getBytes());
@@ -24,38 +25,55 @@ public class JwtUtil {
 
     /**
      * 사용자 이름으로 토큰 발급
+     *
      * @param username
      * @return
      */
-    public String createAccessToken(String username){
-        return generateToken(username,tokenExpiration);
+    public String createAccessToken(String username) {
+        return generateToken(username, tokenExpiration);
     }
-    public String createRefreshToken(String username){
+
+    public String createRefreshToken(String username) {
         return generateToken((username), refreshTokenExpiration);
     }
 
     /**
      * 토큰 생성
+     *
      * @param username
      * @param expiration
      * @return
      */
-    public String generateToken(String username,long expiration){
+    public String generateToken(String username, long expiration) {
         return Jwts.builder()
             .setSubject(username) // 토큰 발행 주체
             .setIssuedAt(new Date()) // 토큰 발행 시간
-            .setExpiration(new Date(System.currentTimeMillis()+expiration)) // 토큰 만료 시간
-            .signWith(secretKey,SignatureAlgorithm.HS256)
+            .setExpiration(new Date(System.currentTimeMillis() + expiration)) // 토큰 만료 시간
+            .signWith(secretKey, SignatureAlgorithm.HS256)
             .compact();
     }
 
-
     /**
-     * JWT 토큰에서 Claims추출 (사용자 정보)
+     * 토큰 유효성 검사 : claims와 userDetails비교
      * @param token
      * @return
      */
-    public Claims extractClaims(String token){
+    public boolean validateToken(String token,UserDetailsImpl userDetails) {
+        try {
+            String username = getUsernameFromToken(token);
+            return (username.equals(userDetails.getUsername())&& !isTokenExpired(token));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * JWT 토큰에서 Claims추출 (사용자 정보)
+     *
+     * @param token
+     * @return
+     */
+    private Claims extractClaims(String token) {
         return Jwts.parserBuilder()
             .setSigningKey(secretKey)
             .build()
@@ -63,17 +81,24 @@ public class JwtUtil {
             .getBody();
     }
 
-    public boolean validateToken(String token){
-        try{
-            extractClaims(token);
-            return true;
-        }
-        catch (Exception e){
-            return false;
-        }
-    }
-
-    public String getUsernameFromToken(String token){
+    /**
+     * Bearer 제거하고 token 보내기
+     *
+     * @param token
+     * @return
+     */
+    public String getUsernameFromToken(String token) {
         return extractClaims(token).getSubject();
     }
+
+    /**
+     * 토큰 만료일 cliam추출
+     * @param token
+     * @return
+     */
+    private boolean isTokenExpired(String token) {
+        Date expiration = extractClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
 }
