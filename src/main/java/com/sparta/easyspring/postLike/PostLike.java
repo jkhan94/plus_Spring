af154a358b4dac4760f5d5c90c8369e45c76aeb9
jkhan94 <hjkwork@naver.com>
2024-06-19 17:52:53 +0900

    update : PostLike 엔티티 수정

--- src/main/java/com/sparta/easyspring/postLike/PostLike.java
+++ src/main/java/com/sparta/easyspring/postLike/PostLike.java
@@ -1,5 +1,34 @@
-package com.sparta.easyspring.postLike;
+package com.sparta.easyspring.postlike;
 
-public class PostLike {
+import com.sparta.easyspring.TimeStamp.TimeStamp;
+import com.sparta.easyspring.auth.entity.User;
+import com.sparta.easyspring.post.Post;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+// TODO User, Post temp 거 사용중
+@Entity
+@Table(name = "postlike")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostLike extends TimeStamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public PostLike(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
 }
