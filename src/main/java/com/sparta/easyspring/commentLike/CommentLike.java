922458227938f7498db14262686836ad8cc674f6
yoodab <yoondab2166@gmail.com>
2024-06-20 09:30:45 +0900

    feat : 댓글 좋아요, 좋아요 해제 구현

--- src/main/java/com/sparta/easyspring/commentLike/CommentLike.java
+++ src/main/java/com/sparta/easyspring/commentLike/CommentLike.java
@@ -1,5 +1,32 @@
 package com.sparta.easyspring.commentLike;
 
-public class CommentLike {
+import com.sparta.easyspring.TimeStamp.TimeStamp;
+import com.sparta.easyspring.auth.entity.User;
+import com.sparta.easyspring.comment.entity.Comment;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+
+
+@Entity
+@Table(name = "commentlikes")
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentLike extends TimeStamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="comment_id")
+    private Comment comment;
+
+    public CommentLike(User user, Comment comment) {
+        this.user = user;
+        this.comment = comment;
+    }
 }
