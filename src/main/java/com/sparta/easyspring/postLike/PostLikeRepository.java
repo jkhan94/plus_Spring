38c610ee1f514cc869da4a20c54d424a46e172c6
jkhan94 <hjkwork@naver.com>
2024-06-20 12:05:28 +0900

    fix : PostLikeRepository 쿼리메소드 수정

--- src/main/java/com/sparta/easyspring/postLike/PostLikeRepository.java
+++ src/main/java/com/sparta/easyspring/postLike/PostLikeRepository.java
@@ -1,7 +1,10 @@
 package com.sparta.easyspring.postlike;
 
+import com.sparta.easyspring.auth.entity.User;
+import com.sparta.easyspring.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PostLikeRepository extends JpaRepository {
-    boolean findByUserAndPost(long userId, long postId);
+public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+    PostLike findByUserAndPost(User user, Post post);
 }
