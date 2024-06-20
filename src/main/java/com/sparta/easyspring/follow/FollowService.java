d38d644af2e9a2bda0a8bc6d3d91f290682e02e7
hongsy521 <seoyeong0521@naver.com>
2024-06-20 17:02:16 +0900

    feat : 자기 자신을 팔로우 불가 예외 처리

--- src/main/java/com/sparta/easyspring/follow/FollowService.java
+++ src/main/java/com/sparta/easyspring/follow/FollowService.java
@@ -12,7 +12,10 @@ public class FollowService {
     private final UserService userService;
 
     public void addFollow(Long followingId, User user) {
-        User followingUser = userService.findById(followingId);
+        User followingUser = userService.findUserById(followingId);
+        if(followingId.equals(user.getId())){
+            throw new IllegalArgumentException("본인을 팔로우 할 수 없습니다.");
+        }
         Follow checkFollow = findFollowById(followingUser.getId(),user);
         if(checkFollow != null){
             throw new IllegalArgumentException("이미 팔로우된 상태입니다.");
@@ -22,7 +25,7 @@ public class FollowService {
     }
 
     public void deleteFollow(Long followingId, User user) {
-        User followingUser = userService.findById(followingId);
+        User followingUser = userService.findUserById(followingId);
         Follow checkFollow = findFollowById(followingUser.getId(),user);
         if(checkFollow == null){
             throw new IllegalArgumentException("취소할 팔로우가 없습니다.");
