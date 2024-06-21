f1b300e7280681051610f428bd8530605ce6a4ab
jkhan94 <hjkwork@naver.com>
2024-06-21 17:31:39 +0900

    test&fix : MockTestDataSetup 추가 및 테스트 파일 수정

--- src/test/java/com/sparta/easyspring/postlike/entity/PostLikeTest.java
+++ src/test/java/com/sparta/easyspring/postlike/entity/PostLikeTest.java
@@ -3,6 +3,7 @@ package com.sparta.easyspring.postlike.entity;
 import com.sparta.easyspring.auth.entity.User;
 import com.sparta.easyspring.auth.entity.UserRoleEnum;
 import com.sparta.easyspring.post.entity.Post;
+import com.sparta.easyspring.postlike.MockTestDataSetup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -19,16 +20,12 @@ class PostLikeTest {
         // given
         String TEST_USER_NAME = "username";
         String TEST_USER_PASSWORD = "password";
-        User TEST_USER = new User(TEST_USER_NAME, TEST_USER_PASSWORD, UserRoleEnum.USER);
-
         String TEST_POST_TITLE = "post title";
         String TEST_POST_CONTENTS = "post contents";
         long TEST_POST_LIKES = 1L;
-        Post TEST_POST = new Post();
-        ReflectionTestUtils.setField(TEST_POST, "title", TEST_POST_TITLE);
-        ReflectionTestUtils.setField(TEST_POST, "contents", TEST_POST_CONTENTS);
-        ReflectionTestUtils.setField(TEST_POST, "likes", TEST_POST_LIKES);
-        ReflectionTestUtils.setField(TEST_POST, "user", TEST_USER);
+
+        User TEST_USER = MockTestDataSetup.mockTestUserSetup();
+        Post TEST_POST = MockTestDataSetup.mockTestPostSetup(TEST_USER);
 
         // when
         postLike = new PostLike(TEST_USER, TEST_POST);
